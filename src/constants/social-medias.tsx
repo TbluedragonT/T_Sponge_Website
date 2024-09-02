83dc2920662cf4ab77c1a7370bd1205bd7e5403b
TbluedragonT <bluedragon319331@gmail.com>
2024-09-02 10:45:51 -0400

    removed facebook and instagram

--- src/constants/social-medias.tsx
+++ src/constants/social-medias.tsx
@@ -1,14 +1,15 @@
-import { Facebook, Instagram, Telegram, Twitter } from "../icons";
+// import { Facebook, Instagram, Telegram, Twitter } from "../icons";
+import { Telegram, Twitter } from "../icons";
 
 export const SOCIALMEDIAS = [
-  {
-    icon: <Facebook className="w-[15px] h-[15px]" />,
-    link: "https://www.facebook.com/share/r6j6hcWjCiLuq6Rw/",
-  },
-  {
-    icon: <Instagram className="w-[15px] h-[15px]" />,
-    link: "https://www.instagram.com/spongeofwallstreet",
-  },
+  // {
+  //   icon: <Facebook className="w-[15px] h-[15px]" />,
+  //   link: "https://www.facebook.com/share/r6j6hcWjCiLuq6Rw/",
+  // },
+  // {
+  //   icon: <Instagram className="w-[15px] h-[15px]" />,
+  //   link: "https://www.instagram.com/spongeofwallstreet",
+  // },
   {
     icon: <Twitter className="w-[15px] h-[15px]" />,
     link: "https://x.com/spongeofwallst",
